19b16e6d1cf1a6250787df2675598e0d8fe670e4
pinkiwinkie <elrumor99@gmail.com>
2024-04-23 12:29:49 +0200

    eureka ok

---
+++ SpringMicroservices/microsevice-user/src/main/java/com/microservice/user/MicroseviceUserApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroseviceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroseviceUserApplication.class, args);
+	}
+
+}
