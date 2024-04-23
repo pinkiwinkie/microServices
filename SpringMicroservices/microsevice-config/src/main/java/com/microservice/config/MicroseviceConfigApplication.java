a0e48024920acd6642b628a576a7ba735e4a9d3b
pinkiwinkie <elrumor99@gmail.com>
2024-04-23 12:14:44 +0200

    config and congig eurke

---
+++ SpringMicroservices/microsevice-config/src/main/java/com/microservice/config/MicroseviceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroseviceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroseviceConfigApplication.class, args);
+	}
+
+}
