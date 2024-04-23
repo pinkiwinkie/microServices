7ecfca9767337b41a331364be9c31d62474e5640
pinkiwinkie <elrumor99@gmail.com>
2024-04-23 11:48:44 +0200

    gateaway

---
+++ SpringMicroservices/microsevice-eureka/src/main/java/com/microservice/eureka/MicroseviceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroseviceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroseviceEurekaApplication.class, args);
+	}
+
+}
---
+++ SpringMicroservices/microsevice-gateaway/src/main/java/com/microservice/gateaway/MicroseviceGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroseviceGateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroseviceGateawayApplication.class, args);
+	}
+
+}
